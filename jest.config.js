9bcd2d1ad71fd5cbe5094a0d7ed644fdd512fff7
周立 <li.zhou@huilianyi.com>
2018-10-26 11:41:22 +0800

    a

--- jest.config.js
+++ jest.config.js
@@ -135,7 +135,8 @@ module.exports = {
   // The glob patterns Jest uses to detect test files
   testMatch: [
     "**/__tests__/**/*.js?(x)",
-    "**/?(*.)+(spec|test).js?(x)"
+      "**/?(*.)+(test).js?(x)"
+    // "**/?(*.)+(spec|test).js?(x)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
